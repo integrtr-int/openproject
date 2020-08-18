ba733231d18e38d03eb05890510ee9d683ab2948
Sharad Chand <pepsighan@sharadchand.com>
2020-08-18 12:43:34 +0545

    feat: hide help menu

--- lib/redmine/menu_manager/top_menu_helper.rb
+++ lib/redmine/menu_manager/top_menu_helper.rb
@@ -40,7 +40,8 @@ module Redmine::MenuManager::TopMenuHelper
   def render_top_menu_right
     content_tag :ul, id: 'account-nav-right', class: 'menu_root account-nav' do
       [render_module_top_menu_node,
-       render_help_top_menu_node,
+      # Do not show help menu.
+      #  render_help_top_menu_node,
        render_user_top_menu_node].join.html_safe
     end
   end
